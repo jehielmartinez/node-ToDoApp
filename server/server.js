b66c08c813e9abfad58bbff3bbe6143196d4bd8c
Jehiel Martinez <jehielmartinez@gmail.com>
2018-11-11 19:00:43 -0600

    add GET all toDos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,16 @@ app.post('/toDos', (req, res) => {
     });
 });
 
+//Get all Todos
+app.get('/toDos', (req,res)=>{
+    toDo.find().then((toDos)=>{
+        res.send({toDos})
+    },(e)=>{
+        res.status(400).send(err);
+    })
+
+})
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
